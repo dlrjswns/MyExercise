c758eba8a1d40adb97c095741f4d8d9be1cb25e6
dlrjswns <leekyunjun@gmail.com>
2022-04-08 21:28:35 +0900

    0.7v LikeListCell LikeButton 알아서 눌러지는 Issue & ExChartCell UI 겹침 Issue 해결

--- MyExercise/View/Cell/ExLikeListCell.swift
+++ MyExercise/View/Cell/ExLikeListCell.swift
@@ -7,44 +7,80 @@
 
 import UIKit
 
+struct Comment {
+    static let chestComment = "계획된 가슴운동이 없습니다"
+    static let backComment = "계획된 등운동이 없습니다"
+    static let shoulderComment = "계획된 어깨운동이 없습니다"
+    static let legComment = "계획된 다리운동이 없습니다"
+    static let armComment = "계획된 팔운동이 없습니다"
+}
+
+class ExerciseLabel: UILabel {
+    
+    init(comment: String) {
+        super.init(frame: .zero)
+        configureUI()
+        text = comment
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        textColor = .label
+        font = UIFont.systemFont(ofSize: 15, weight: .medium)
+    }
+}
+
 class ExLikeListCell: UITableViewCell {
     
     static let identifier = "ExLikeListCell"
     
-    private let dayExerciseLabel: UILabel = {
-       let label = UILabel()
-        label.textColor = .label
-        label.text = "dfdf"
-        return label
-    }()
+//    private let chestExerciseLabel: UILabel = {
+//       let label = UILabel()
+//        label.textColor = .label
+//        label.text = Comment.chestComment
+//        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+//        return label
+//    }()
+    private let chestExerciseLabel = ExerciseLabel(comment: Comment.chestComment)
+    private let backExerciseLabel = ExerciseLabel(comment: Comment.backComment)
+    private let shoulderExerciseLabel = ExerciseLabel(comment: Comment.shoulderComment)
+    private let legExerciseLabel = ExerciseLabel(comment: Comment.legComment)
+    private let armExerciseLabel = ExerciseLabel(comment: Comment.armComment)
     
-    private let dayExerciseLabel1: UILabel = {
-       let label = UILabel()
-        label.textColor = .label
-        label.text = "dfdf"
-        return label
-    }()
+//    private let backExerciseLabel: UILabel = {
+//       let label = UILabel()
+//        label.textColor = .label
+//        label.text = Comment.backComment
+//        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+//        return label
+//    }()
     
-    private let dayExerciseLabel2: UILabel = {
-       let label = UILabel()
-        label.textColor = .label
-        label.text = "dfdf"
-        return label
-    }()
+//    private let shoulderExerciseLabel: UILabel = {
+//       let label = UILabel()
+//        label.textColor = .label
+//        label.text = Comment.shoulderComment
+//        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+//        return label
+//    }()
     
-    private let dayExerciseLabel3: UILabel = {
-       let label = UILabel()
-        label.textColor = .label
-        label.text = "dfdf"
-        return label
-    }()
-    
-    private let dayExerciseLabel4: UILabel = {
-       let label = UILabel()
-        label.textColor = .label
-        label.text = "dfdf"
-        return label
-    }()
+//    private let legExerciseLabel: UILabel = {
+//       let label = UILabel()
+//        label.textColor = .label
+//        label.text = Comment.legComment
+//        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+//        return label
+//    }()
+//
+//    private let armExerciseLabel: UILabel = {
+//       let label = UILabel()
+//        label.textColor = .label
+//        label.text = Comment.armComment
+//        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+//        return label
+//    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -57,33 +93,37 @@ class ExLikeListCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        dayExerciseLabel.text = nil
-       
+        chestExerciseLabel.text = nil
+        backExerciseLabel.text = nil
+        shoulderExerciseLabel.text = nil
+        legExerciseLabel.text = nil
+        armExerciseLabel.text = nil
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         print("contentViewFrame = \(contentView.frame.height/6)")
-        dayExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
-        dayExerciseLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
-        dayExerciseLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+        chestExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        chestExerciseLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        chestExerciseLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
         
-        dayExerciseLabel1.translatesAutoresizingMaskIntoConstraints = false
-        dayExerciseLabel1.topAnchor.constraint(equalTo: dayExerciseLabel.bottomAnchor, constant: 30).isActive = true
-        dayExerciseLabel1.leftAnchor.constraint(equalTo: dayExerciseLabel.leftAnchor).isActive = true
+        backExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        backExerciseLabel.topAnchor.constraint(equalTo: chestExerciseLabel.bottomAnchor, constant: 10).isActive = true
+        backExerciseLabel.leftAnchor.constraint(equalTo: chestExerciseLabel.leftAnchor).isActive = true
         
-        dayExerciseLabel2.translatesAutoresizingMaskIntoConstraints = false
-        dayExerciseLabel2.topAnchor.constraint(equalTo: dayExerciseLabel1.bottomAnchor, constant: 30).isActive = true
-        dayExerciseLabel2.leftAnchor.constraint(equalTo: dayExerciseLabel1.leftAnchor).isActive = true
+        shoulderExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        shoulderExerciseLabel.topAnchor.constraint(equalTo: backExerciseLabel.bottomAnchor, constant: 10).isActive = true
+        shoulderExerciseLabel.leftAnchor.constraint(equalTo: backExerciseLabel.leftAnchor).isActive = true
         
-        dayExerciseLabel3.translatesAutoresizingMaskIntoConstraints = false
-        dayExerciseLabel3.topAnchor.constraint(equalTo: dayExerciseLabel2.bottomAnchor, constant: 30).isActive = true
-        dayExerciseLabel3.leftAnchor.constraint(equalTo: dayExerciseLabel2.leftAnchor).isActive = true
+        legExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        legExerciseLabel.topAnchor.constraint(equalTo: shoulderExerciseLabel.bottomAnchor, constant: 10).isActive = true
+        legExerciseLabel.leftAnchor.constraint(equalTo: shoulderExerciseLabel.leftAnchor).isActive = true
         
-        dayExerciseLabel4.translatesAutoresizingMaskIntoConstraints = false
-        dayExerciseLabel4.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30).isActive = true
-        dayExerciseLabel4.leftAnchor.constraint(equalTo: dayExerciseLabel3.leftAnchor).isActive = true
-//        dayExerciseLabel4.topAnchor.constraint(equalTo: dayExerciseLabel3.bottomAnchor, constant: contentView.frame.height/6).isActive = true
+        armExerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+//        dayExerciseLabel4.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30).isActive = true
+        armExerciseLabel.topAnchor.constraint(equalTo: legExerciseLabel.bottomAnchor, constant: 10).isActive = true
+        armExerciseLabel.leftAnchor.constraint(equalTo: legExerciseLabel.leftAnchor).isActive = true
+//        dayExerciseLabel4.topAnchor.constraint(equalTo: dayExerciseLabel3.bottomAnchor, constant: -30).isActive = true
         
     }
     
@@ -91,63 +131,57 @@ class ExLikeListCell: UITableViewCell {
     private func configureCell() {
         self.accessoryType = .disclosureIndicator
 //        print("contentViewFrame = \(contentView.frame)")
-        contentView.addSubview(dayExerciseLabel)
+        contentView.addSubview(chestExerciseLabel)
         
         
-        contentView.addSubview(dayExerciseLabel1)
+        contentView.addSubview(backExerciseLabel)
        
         
-        contentView.addSubview(dayExerciseLabel2)
+        contentView.addSubview(shoulderExerciseLabel)
        
         
-        contentView.addSubview(dayExerciseLabel3)
+        contentView.addSubview(legExerciseLabel)
     
         
-        contentView.addSubview(dayExerciseLabel4)
+        contentView.addSubview(armExerciseLabel)
         
         
     }
     
     public func configureCell(with model: DayExercise) { // 일단 model은 잘 들어옴
-        print("mmmm = \(model)")
         if let armExercises = model.armExercise {
             //Arm exercise
             _ = armExercises.map { armExercise in
-                dayExerciseLabel.text = armExercise.rawValue
+                chestExerciseLabel.text = armExercise.rawValue
             }
-            return
         }
         
         if let shoulderExercises = model.shoulderExercise {
             //Shoulder exercise
             _ = shoulderExercises.map { shoulderExercise in
-                dayExerciseLabel.text = shoulderExercise.rawValue
+                backExerciseLabel.text = shoulderExercise.rawValue
             }
-            return
         }
         
         if let backExercises = model.backExercise {
             //Back exercise
             _ = backExercises.map { backExercise in
-                dayExerciseLabel.text = backExercise.rawValue
+                shoulderExerciseLabel.text = backExercise.rawValue
             }
-            return
         }
         
         if let chestExercises = model.chestExercise {
             //Chest exercise
             _ = chestExercises.map { chestExercise in
-                dayExerciseLabel.text = chestExercise.rawValue
+                legExerciseLabel.text = chestExercise.rawValue
             }
-            return
         }
         
         if let legExercises = model.legExercise {
             //Leg exercise
             _ = legExercises.map { legExercise in
-                dayExerciseLabel.text = legExercise.rawValue
+                armExerciseLabel.text = legExercise.rawValue
             }
-            return
         }
         
     }
